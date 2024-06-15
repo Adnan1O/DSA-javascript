6568757237711783fe3eefe3ff7d75a60a3a63bf
Adnan1O <adnn4u@gmail.com>
2024-06-15 17:13:33 +0530

    push

---
+++ day6.js
@@ -0,0 +1,52 @@
+// Input: 123
+// Output: 321
+
+// Input: -123
+// Output: -321
+
+// Input: 120
+// Output: 21
+
+// const reverseIt=(num)=>{
+//     const rev = num.toString().split('').reverse().join('')
+//     return rev
+// }
+// console.log(reverseIt(123))
+
+
+// function mergeInOrder({l1,l2}){
+//     const newArray = l1.concat(l2).sort()
+//     const sort = newArray.sort()
+//     return newArray
+// }
+
+
+// const  l1 = [1, 2, 4]
+// const  l2 = [1, 3, 4]
+// console.log(mergeInOrder({l1,l2}))
+
+
+const find=(q)=>{
+    const sample ={'(':')', '[':']', '{':'}' }
+    const split = q.split('')
+    split.map((words,i =>{
+        if (words[i].includes(sample)) {
+            console.log("yes")
+        }
+        
+    }))
+        
+    
+}
+
+
+const q = "()[]{}"
+console.log(find(q))
+
+// Input: "()"
+// Output: true
+
+// Input: "()[]{}"
+// Output: true
+
+// Input: "(]"
